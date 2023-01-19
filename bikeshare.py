a44fdda9690e052e7145a703d6539ad33e3d4197
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-01-20 00:06:25 +0500

    push
    push

--- bikeshare.py
+++ bikeshare.py
@@ -17,30 +17,26 @@ DAYS = ['sunday', 'monday', 'tuesday', 'wednesday',
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
-
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+
+    print('\nHello! Let\'s explore some US bikeshare data!')
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+
     while True:
-       city = input('Which city do you want to explore Chicago, New York or Washington? \n> ').lower()
-       if city in CITIES:
-           break
-
-    # get user input for month (all, january, february, ... , june)
-    month = get_user_input('All right! now it\'s time to provide us a month name '\
-                    'or just say \'all\' to apply no month filter. \n(e.g. all, january, february, march, april, may, june) \n> ', MONTHS)
-
-    # get user input for day of week (all, monday, tuesday, ... sunday)
-    day = get_user_input('One last thing. Could you type one of the week day you want to analyze?'\
-                   ' You can type \'all\' again to apply no day filter. \n(e.g. all, monday, sunday) \n> ', DAYS)
-     
+        city = input ("Please input the name of the city that you would like toanalyze: ").lower()
+    
+        # get user input for month (all, january, february, ... , june)
+        month = input ("Please input the month that you would like to filter by, ortype 'all' to apply no month filter: ").lower()
+    
+        # get user input for day of week (all, monday, tuesday, ... sunday)
+        day = input ("Please input the day of the week that you would like to filterby, or type 'all' to apply no day filter: ").lower()
+        break
     print('-'*40)
     return city, month, day
-
 
 def load_data(city, month, day):
     cities =CITY_DATA['ci']
